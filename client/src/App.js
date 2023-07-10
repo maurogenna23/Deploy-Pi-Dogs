cc860fc53d4d607e481ddb2e4f4b6ac7a9eb3802
mauro_genna <gennamauro18@gmail.com>
2023-07-10 14:08:27 -0300

    carpeta build

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,29 @@
 import './App.css';
-import  { Routes, Route, useLocation} from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import LandingPage from './views/LandingPage/landingPage';
 import Home from './views/Home/Home';
 import Create from './views/Create/create';
 import Detail from './views/Detail/detail';
 import NavBar from './components/NavBar/NavBar';
 import Error from './components/Error/Error'
-import About from './views/About/About'; 
-
+import About from './views/About/About';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:3001/'
 function App() {
 
   const location = useLocation();
 
   return (
     <div className="App">
-        {location.pathname !== "/" && location.pathname !== "/home" && location.pathname !== "/about" ? <NavBar/> : null} 
-        <Routes>
-          <Route path='*' element={<Error/>}/>
-          <Route path='/' element={<LandingPage/>}/>
-          <Route path='/about' element={<About/>} />
-          <Route path='/home' element= {<Home/>}/>
-          <Route path='/home/:id' element={<Detail/>} />
-          <Route path='/create' element={<Create/>} />
-        </Routes>
+      {location.pathname !== "/" && location.pathname !== "/home" && location.pathname !== "/about" ? <NavBar /> : null}
+      <Routes>
+        <Route path='*' element={<Error />} />
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/home/:id' element={<Detail />} />
+        <Route path='/create' element={<Create />} />
+      </Routes>
     </div>
   );
 }
